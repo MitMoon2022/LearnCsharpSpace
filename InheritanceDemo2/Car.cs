6746061543503464bf74f5306122545ee75d595d
mrmoon <jeffio_99@yahoo.com>
2022-07-30 20:39:32 +0800

    Added InterfaceQuiz , learn about interface and inheritance. Added InheritanceDemo2.csproj

---
+++ InheritanceDemo2/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo2
+{
+    //implementing the interface IDestroyable
+    class Car : Vehicle, IDestroyable
+    {
+        //implementing the interface's property
+        public string DestructionSound { get; set; }
+
+        //creating a new property to store the destroyable objects nearby
+        //when a car gets destroyed it should also destroy the nearby object
+        //this list is of type IDestroyable which means it can store any object
+        //that implements this interface and we can only access the properties and
+        //methods in this interface
+        // public List&lt;IDestroyable &gt;DestoryablesNearby;
+        public List<IDestroyable> DestoryablesNearby;
+            
+    public Car(float speed, string color)
+        {
+            this.Speed = speed;
+            this.Color = color;
+            //initialize the interface's property with a value in the constructor
+            DestructionSound = "CarExplosionSound.mp3";
+            //initialize the list of destroyable objects
+            DestoryablesNearby = new List<IDestroyable>();
+        }
+
+        //implementing the interface's method
+        public void Destroy()
+        {
+            //when a car gets destroyed we should play the destruction sound
+            //and create fire effect
+            Console.WriteLine("Playing destruction sound {0}", DestructionSound);
+            Console.WriteLine("Create fire");
+            //go through each destoryable object nearby and call it's destroy method
+            foreach (IDestroyable destroyable in DestoryablesNearby)
+            {
+                destroyable.Destroy();
+            }
+
+        }
+
+    }
+
+}
---
+++ InheritanceDemo2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InheritanceDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Program!");
+            //creating two objects of type chair
+            Chair officeChair = new Chair("Brown", "Plastic");
+            Chair gamingChair = new Chair("Red", "Wood");
+            //creating a new object of type car
+            Car damagedCar = new Car(80f, "Blue");
+            //add the two chairs to the IDestroyable list of the car
+            //so that when we destroy the car the destroyable objects
+            //that are near the car will get destroyed as well
+            damagedCar.DestoryablesNearby.Add(officeChair);
+            damagedCar.DestoryablesNearby.Add(gamingChair);
+            //destory the car
+            damagedCar.Destroy();
+            //pause
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
