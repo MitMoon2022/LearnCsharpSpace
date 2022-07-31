c35d89845ecba70faac2decf2a76119c51619f36
mrmoon <jeffio_99@yahoo.com>
2022-07-31 16:30:51 +0800

    IEnumberableEx1 coding exercise IEnumberableInterface.zip download from C#corner. https://www.c-sharpcorner.com/uploadfile/0c1bb2/ienumerable-interface-in-c-sharp/ Added the next lecture - 0902+-+PolymorphismC.zip.

---
+++ IEnumerableEx1/Program.cs
@@ -0,0 +1,73 @@
+﻿//IEnumerable
+//This time, you have to create Contact and PhoneBook classes in the right way that will allow 
+//you to run the Main method we provided.
+
+//Contacts = new List<Contact>{
+//                new Contact("Andre", "435797087"),
+//                new Contact("Lisa", "435677087"),
+//                new Contact("Dine", "3457697087"),
+//                new Contact("Sofi", "4367697087")
+//            };
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace IEnumerableEx1
+{
+    class Contact
+    {
+        // solution
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public void Call()
+        {
+            System.Console.WriteLine("Calling to {0}. Phone number is {1}", Name, Phone);
+        }
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+        // solution
+    }
+
+    class PhoneBook : IEnumerable<Contact>
+    {
+
+        public List<Contact> Contacts;
+        public PhoneBook()
+        {
+            Contacts = new List<Contact>{
+                new Contact("Andre", "435797087"),
+                new Contact("Lisa", "435677087"),
+                new Contact("Dine", "3457697087"),
+                new Contact("Sofi", "4367697087")
+            };
+        }
+        // solution
+        IEnumerator<Contact> IEnumerable<Contact>.GetEnumerator()
+        {
+            return Contacts.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        // solution
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of Program!");
+            PhoneBook MyPhoneBook = new PhoneBook();
+            foreach (Contact contact in MyPhoneBook)
+            {
+                contact.Call();
+            }
+            Console.ReadKey();
+        }
+    }
+}
