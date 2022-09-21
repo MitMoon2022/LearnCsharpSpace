9c3643ea5c1aaf47f26f05355ec91c72b9123e4f
jeffiotan <jeffiotan@mit.com.sg>
2022-09-21 16:16:23 +0800

    MasterC# ReadWriteAssignment and learn String line.Split() Read from file and check() then write into a file. Basic. https://www.c-sharpcorner.com/article/working-with-c-sharp-streamreader/ https://www.c-sharpcorner.com/article/csharp-streamwriter-example/

---
+++ ReadWriteAssignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace ReadWriteAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start!");
+
+            // File name  
+            string fileName = @"D:\jftMit1\Master_C#\input.txt";
+            try
+            {
+                // Create a StreamReader  
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    // Read line by line  
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(line);
+                        //Modifier start here!
+                        using (StreamWriter file = new StreamWriter(@"D:\jftMit1\Master_C#\output.txt",true))
+                        {
+                            if(line.Contains("split"))
+                            {
+                                //file.WriteLine("T");
+                                Console.Write(line.Split()[4] + " ");
+                                file.Write(line.Split()[4] + " ");
+
+                                //string[] subs = line.Split("split");        //s.Split(' ', '.');
+                                //for(int i = 0; i<subs.Length; i++)          //foreach (var sub in subs)
+                                //{
+                                //    Console.WriteLine($"Substring[]" + i +" : " +subs[i]);
+                                //    file.WriteLine(subs[i]);
+                                //}
+                                //Console.WriteLine("=================================");
+
+
+                            }
+                           
+                        }       
+                    }//endof_while
+                    Console.WriteLine("==================================================");
+                    
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }//end_class_program
+}
